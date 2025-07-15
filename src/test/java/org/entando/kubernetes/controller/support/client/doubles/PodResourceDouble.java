c8c64d7a0a180f0fd2f559eea9903defeb3028ab
Walter Caffiero <w.caffiero@entando.com>
2025-07-15 03:01:19 +0200

    ENG-5551 fixed test with migration to new fabric8

--- src/test/java/org/entando/kubernetes/controller/support/client/doubles/PodResourceDouble.java
+++ src/test/java/org/entando/kubernetes/controller/support/client/doubles/PodResourceDouble.java
@@ -51,7 +51,7 @@ public class PodResourceDouble extends PodOperationsImpl {
 
     public PodResourceDouble(PodOperationContext podOperationContext, ClientContext clientContext, String namespace) {
         super(podOperationContext, HasMetadataOperationsImpl.defaultContext(clientContext).withNamespace(namespace));
-        this.clientContext=clientContext;
+        this.clientContext = clientContext;
     }
 
     @Override
